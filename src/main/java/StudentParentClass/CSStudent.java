df80da3ab52bd9a80af239c7515f6d34483a42f7
Nicolai Kopka <nicolai.kopka@aol.de>
2022-05-24 15:08:33 +0200

    added Student as abstract class

--- src/main/java/StudentParentClass/CSStudent.java
+++ src/main/java/StudentParentClass/CSStudent.java
@@ -5,12 +5,17 @@ import java.util.UUID;
 public class CSStudent extends Student {
 
 
-    public CSStudent(String name) {
-        super(name);
+    public CSStudent(String name, int numberOfModules) {
+        super(name, numberOfModules);
     }
 
     @Override
     public String returnSubject() {
         return "I am a CS student!";
+    }
+
+    @Override
+    public int returnNumberOfModules() {
+        return super.numberOfModules;
     }
 }
