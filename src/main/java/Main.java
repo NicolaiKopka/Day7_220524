bb847a0aee5839a89f009687a33a5ebac2e34ea5
Nicolai Kopka <nicolai.kopka@aol.de>
2022-05-25 20:42:18 +0200

    implemented HashMap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,29 +5,21 @@ import StudentParentClass.StudentDB;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-//        Student student1 = new CSStudent("Raj");
-//        Student student2 = new CSStudent("Mohini");
-//        Student student3 = new HistoryStudent("Anjali");
-//        Student[] studentArray = {student1, student2, student3};
-//        var studentDB = new StudentDB(studentArray);
-//        System.out.println(studentDB.toString());
-//        studentDB.removeStudent(2);
-//        System.out.println(studentDB.toString());
+
 
         Student student1 = new CSStudent("Raj", 15);
         Student student2 = new HistoryStudent("Mohini", 22);
         List<Student> studentArray = new ArrayList<>(List.of(student1, student2));
         var studentDB = new StudentDB(studentArray);
-        List<Student> students = studentDB.list();
-        System.out.println(students.get(0).returnSubject());
-        System.out.println(student1.returnNumberOfModules());
-        System.out.println(students.get(1).returnSubject());
-        System.out.println(student2.returnNumberOfModules());
+        Collection<Student> students = studentDB.list();
+        System.out.println(studentDB.toString());
+
 
     }
 }
--- src/main/java/StudentParentClass/StudentDB.java
+++ src/main/java/StudentParentClass/StudentDB.java
@@ -1,40 +1,48 @@
 package StudentParentClass;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class StudentDB {
 
-    private List<Student> studentsInDB;
+    private Map<String, Student> studentsInDB;
 
     public StudentDB(List<Student> students) {
-        this.studentsInDB = new ArrayList<>(students);
+        this.studentsInDB = new HashMap<>();
+        for (Student student : students) {
+            this.studentsInDB.put(student.getId(), student);
+        }
     }
 
-    public List<Student> list() {
-        return studentsInDB;
+    public Collection<Student> list() {
+        return studentsInDB.values();
     }
 
     public Student randomStudent(){
         int randomIndex = (int) (Math.random() * studentsInDB.size());
-        return studentsInDB.get(randomIndex);
+        return new ArrayList<>(studentsInDB.values()).get(randomIndex);
     }
 
     @Override
     public String toString() {
-        String allStudents = "";
-        for (Student student : studentsInDB) {
-            allStudents += student.getName() + ": " + student.getId() + ";\n";
+        StringBuilder allStudents = new StringBuilder();
+        for (Map.Entry<String, Student> entry: studentsInDB.entrySet()) {
+            allStudents.append("ID: ");
+            allStudents.append(entry.getKey());
+            allStudents.append(": ");
+            allStudents.append(entry.getValue());
+            allStudents.append(";\n");
         }
-        return allStudents;
+        return allStudents.toString();
     }
-    public void addStudent(Student student) {
-        studentsInDB.add(student);
+    public void addStudent(String id, Student student) {
+        if(studentsInDB.containsKey(id)) {
+            throw new RuntimeException("Student already exists");
+        }
+        studentsInDB.putIfAbsent(id, student);
     }
 
-    public void removeStudent(int position) {
-        studentsInDB.remove(position - 1);
+    public void removeStudent(String id) {
+        studentsInDB.remove(id);
     }
 }
